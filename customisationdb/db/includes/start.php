e9944abf4d13b3f06b269410534f732cf341cc01
Paul Sohier <paul999@phpbb.com>
2008-03-22 13:59:17 +0000

    Use phpBB.com compitable code in repo for now.

--- customisationdb/db/includes/start.php
+++ customisationdb/db/includes/start.php
@@ -13,19 +13,14 @@ if (!defined('IN_PHPBB'))
 	exit;
 }
 
-$phpEx = '';
-$phpbb_root_path = '../../community';
 $root_path = '../../';
 
-include("{$phpbb_root_path}common.php");
+// For phpBB.com, normally include just common.php
+include("{$phpbb_root}common.php");
+
 include("{$root_path}db/includes/class_main.$phpEx");
 include("{$root_path}db/includes/class_hooks.$phpEx");
 include("{$root_path}db/includes/class_api.$phpEx");
-
-// Start session management
-$user->session_begin();
-$auth->acl($user->data);
-$user->setup();
 
 // Now we init the template (this will later be replaced by the real template code)
 $template->set_custom_template($phpbb_root_path . '../db/template/', 'website');
