7114a1e0daadc6a11ac118b39cde3907b9c5065f
Andreas Fischer <bantu@phpbb.com>
2008-07-04 15:15:43 +0000

    Setting svn properties. Because phpBB does use LF we should use it, too. Add mime-type so files are correctly handled by the webserver.

--- titania/install/index.php
+++ titania/install/index.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package Titania
-* @version $Id:$
+* @version $Id$
 * @copyright (c) 2008 phpBB Customisation Database Team
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
