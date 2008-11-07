8971fef918269d1917ee7973b3d7a446de4b91bf
Darek Maciejewski <edaroo@gmail.com>
2008-11-07 22:10:08 +0000

    faq class created

---
+++ titania/includes/mods/faq_main.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ *
+ * @package titania
+ * @version $Id: faq_main.php 122 2008-11-07 20:20:10Z daroPL $
+ * @copyright (c) 2008 phpBB Customisation Database Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* faq_main
+* Class for FAQ module
+* @package mods
+*/
+class faq_main extends titania_object
+{
+	public $p_master;
+	public $u_action;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct(&$p_master)
+	{
+		global $user;
+
+		$this->p_master = &$p_master;
+
+		$this->page = $user->page['script_path'] . $user->page['page_name'];
+	}
+
+	/**
+	 * main method for this module
+	 *
+	 * @param string $id
+	 * @param string $mode
+	 */
+	public function main($id, $mode)
+	{
+		global $user, $template, $cache;
+
+		$user->add_lang(array('titania_faq'));
+
+		$faq_id		= request_var('faq_id', 0);
+		$submit		= isset($_POST['submit']) ? true : false;
+	}
+
+	/**
+	 * create a faq list for specific contrib
+	 *
+	 * @param int $contrib_id
+	 */
+	private function faq_list($contrib_id)
+	{
+		
+	}
+}
