4abd7294b0b58a4ff6c18b9c89ad10a49b5fe387
Nathan Guse <exreaction@phpbb.com>
2009-06-27 05:37:13 +0000

    Had the order backwards in the query

--- titania/includes/objects/faq.php
+++ titania/includes/objects/faq.php
@@ -101,7 +101,7 @@ class titania_faq extends titania_database_object
 	{
 		$sql = 'SELECT order_id FROM ' . TITANIA_CONTRIB_FAQ_TABLE . '
 			WHERE faq_order_id ' . (($direction == 'up') ? '<' : '>') . $this->faq_order_id . '
-			ORDER BY faq_order_id ' . (($direction == 'up') ? 'ASC' : 'DESC');
+			ORDER BY faq_order_id ' . (($direction == 'up') ? 'DESC' : 'ASC');
 		phpbb::$db->sql_query_limit($sql, 1);
 		$new_order_id = phpbb::$db->sql_fetchfield('order_id');
 		phpbb::$db->sql_freeresult();
