96f8912ae2aeb4be4f0f5e9baa7c7fe9e8090e5c
Nathan Guse <exreaction@phpbb.com>
2009-11-21 04:14:04 +0000

    Missed this when reverting somehow

--- titania/index.php
+++ titania/index.php
@@ -19,8 +19,14 @@ require TITANIA_ROOT . 'common.' . PHP_EXT;
 // Get the category_id
 $category = request_var('c', '');
 $category_ary = explode('-', $category);
-
-$category_id = ($category_ary) ? (int) array_pop($category_ary) : (int) $category;
+if ($category_ary)
+{
+	$category_id = array_pop($category_ary);
+}
+else
+{
+	$category_id = (int) $category;
+}
 
 $action = request_var('action', '');
 
@@ -30,41 +36,70 @@ switch ($action)
 	* Rate something & remove a rating from something
 	*/
 	case 'rate' :
-		$rating = new titania_rating();
-		$rating->determine_rating();
+		$type = request_var('type', '');
+		$id = request_var('id', 0);
+		$value = request_var('value', -1.0);
+
+		switch ($type)
+		{
+			case 'author' :
+				titania::load_object('author');
+				$object = new titania_author();
+				$object->load($id);
+				$redirect = $object->get_url();
+
+				if (!$object)
+				{
+					trigger_error('AUTHOR_NOT_FOUND');
+				}
+			break;
+
+			case 'contrib' :
+				titania::load_object('contribution');
+				$object = new titania_contribution();
+				$object->load($id);
+				$redirect = $object->get_url();
+
+				if (!$object)
+				{
+					trigger_error('CONTRIB_NOT_FOUND');
+				}
+			break;
+
+			default :
+				trigger_error('BAD_RATING');
+			break;
+		}
+
+		titania::load_object('rating');
+		$rating = new titania_rating($type, $object);
+		$rating->load();
+
+		$result = ($value == -1) ? $rating->delete_rating() : $rating->add_rating($value);
+		if ($result)
+		{
+			redirect($redirect);
+		}
+		else
+		{
+			trigger_error('BAD_RATING');
+		}
 	break;
 
 	/**
 	* Default (display category/contrib list)
 	*/
 	default :
-		if ($category_id)
+		titania_display_categories($category_id);
+
+		if ($category_id != 0)
 		{
-			titania::add_lang('contributions');
 			titania_display_contribs('category', $category_id);
-
-			phpbb::$template->assign_vars(array(
-				'U_CREATE_CONTRIBUTION'		=> (phpbb::$auth->acl_get('titania_contrib_submit')) ? titania::$url->build_url('contributions/create') : '',
-			));
 		}
-		else
-		{
-			$categories = titania::$cache->get_categories();
 
-			$tag = new titania_tag();
-
-			foreach ($categories as $cat_id => $row)
-			{
-				$tag->__set_array($row);
-
-				if (!phpbb::$config['titania_display_empty_cats'] && !$tag->tag_items)
-				{
-					continue;
-				}
-
-				phpbb::$template->assign_block_vars('categories', $tag->assign_display(true));
-			}
-		}
+		phpbb::$template->assign_vars(array(
+			'U_CREATE_CONTRIBUTION'		=> (phpbb::$auth->acl_get('titania_contrib_submit')) ? titania::$url->build_url('contributions/create') : '',
+		));
 	break;
 }
 
