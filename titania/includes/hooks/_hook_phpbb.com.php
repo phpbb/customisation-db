26b6c07cdc1a03358164bb9c1adaaf47f5b9f4cb
Nathan Guse <exreaction@phpbb.com>
2010-04-04 22:07:42 +0000

    footer as well

--- titania/includes/hooks/_hook_phpbb.com.php
+++ titania/includes/hooks/_hook_phpbb.com.php
@@ -17,6 +17,7 @@ if (!defined('IN_TITANIA'))
 }
 
 titania::$hook->register('titania_page_header', 'phpbb_com_header');
+titania::$hook->register('titania_page_footer', 'phpbb_com_footer');
 
 function phpbb_com_header($page_title)
 {
@@ -25,8 +26,23 @@ function phpbb_com_header($page_title)
 	phpbb::$template->set_custom_template(TITANIA_ROOT . '../../template/');
 	phpbb::$template->set_filenames(array(
 		'phpbb_com_header'		=> 'overall_header.html',
+		'phpbb_com_footer'		=> 'overall_footer.html',
 	));
 	phpbb::$template->assign_display('phpbb_com_header', 'PHPBB_COM_HEADER', false);
+	phpbb::$template->assign_display('phpbb_com_footer', 'PHPBB_COM_FOOTER', false);
+
+	titania::set_custom_template();
+}
+
+function phpbb_com_footer($run_cron, $template_body)
+{
+	// Setup the phpBB.com footer
+	include(TITANIA_ROOT . '../../vars.' . PHP_EXT);
+	phpbb::$template->set_custom_template(TITANIA_ROOT . '../../template/');
+	phpbb::$template->set_filenames(array(
+		'phpbb_com_footer'		=> 'overall_footer.html',
+	));
+	phpbb::$template->assign_display('phpbb_com_footer', 'PHPBB_COM_FOOTER', false);
 
 	titania::set_custom_template();
 }
