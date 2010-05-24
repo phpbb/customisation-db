503e64342c001bc968375f5199dfcdcad48dbc36
Mickaël SALFATI <elglobo@phpbb-services.com>
2010-05-24 23:12:27 +0000

    Code more simple with less queries. We select now attachment_table in $sql_ary. Fix issue with description and BBcodes. There is always issue with smiley and storage in POSTS_TABLE.

--- titania/includes/manage_tools/update_release_topics.php
+++ titania/includes/manage_tools/update_release_topics.php
@@ -48,7 +48,8 @@ class update_release_topics
 
 		// Greb our batch
 		$sql_ary = array(
-			'SELECT'	=> 'c.contrib_id, c.contrib_user_id, r.revision_id, r.attachment_id, r.revision_version, p.post_id',
+			'SELECT'	=> 'c.contrib_id, c.contrib_user_id, c.contrib_type, c.contrib_name, c.contrib_name_clean, c.contrib_desc, c.contrib_desc_uid, c.contrib_release_topic_id,
+				r.revision_id, r.attachment_id, r.revision_version, p.post_id, a.real_filename, a.filesize',
 			
 			'FROM'		=> array(
 				TITANIA_CONTRIBS_TABLE	=> 'c',
@@ -56,10 +57,12 @@ class update_release_topics
 				POSTS_TABLE				=> 'p',
 				TOPICS_TABLE			=> 't',
 				USERS_TABLE				=> 'u',
+				TITANIA_ATTACHMENTS_TABLE => 'a',
 			),
 			
 			'WHERE'		=> "t.topic_id = p.topic_id AND u.user_id = c.contrib_user_id 
 				AND c.contrib_release_topic_id = t.topic_id AND c.contrib_id = r.contrib_id
+				AND r.attachment_id = a.attachment_id
 				AND t.topic_first_post_id  = p.post_id",
 		);
 		
@@ -67,30 +70,33 @@ class update_release_topics
 		$result	= phpbb::$db->sql_query_limit($sql, $this->step_size, $start);
 		$batch	= phpbb::$db->sql_fetchrowset($result);
 		phpbb::$db->sql_freeresult($result);
-
+		
 		if (sizeof($batch))
 		{
 			// Walk through the batch
 			foreach ($batch as $this->post)
 			{
 				// Get user data
-				$user_ids = array($this->post['contrib_user_id']);
-				users_overlord::load_users($user_ids);
+				users_overlord::load_users(array($this->post['contrib_user_id']));
 				
-				// Load the contribution
 				$contrib = new titania_contribution();
-				$contrib->load((int) $this->post['contrib_id']);
-				$contrib->get_download($this->post['revision_id']);
+				$contrib->author = new titania_author();
+				
+				$contrib->__set_array($this->post);
+				$contrib->author->user_id = $contrib->contrib_user_id;
+				
+				$contrib_desc = censor_text($contrib->contrib_desc);
+				titania_decode_message($contrib_desc, $contrib->contrib_desc_uid);
 				
 				$body = sprintf(phpbb::$user->lang[titania_types::$types[$contrib->contrib_type]->create_public],
 					$contrib->contrib_name,
 					$contrib->author->get_url(),
 					users_overlord::get_user($contrib->author->user_id, '_username'),
-					titania_decode_message($contrib->contrib_desc, $contrib->contrib_desc_uid),
-					$this->post['revision_version'],
+					$contrib_desc,
+					$contrib->revision_version,
 					titania_url::build_url('download', array('id' => $this->post['attachment_id'])),
-					$contrib->download['real_filename'],
-					$contrib->download['filesize'],
+					$contrib->real_filename,
+					$contrib->filesize,
 					$contrib->get_url(),
 					$contrib->get_url('support')
 				);
@@ -99,7 +105,7 @@ class update_release_topics
 					'poster_id'		=> titania_types::$types[$contrib->contrib_type]->forum_robot,
 					'forum_id' 		=> titania_types::$types[$contrib->contrib_type]->forum_database,
 					'topic_id'		=> $contrib->contrib_release_topic_id,
-					'post_id'		=> $this->post['post_id'],
+					'post_id'		=> $contrib->post_id,
 					'topic_title'	=> $contrib->contrib_name,
 					'post_text'		=> $body,
 				);
