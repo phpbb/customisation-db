c4e92b2d0504cc071158895762c27f627eb1501a
Mickaël SALFATI <elglobo@phpbb-services.com>
2010-05-24 23:44:07 +0000

    Some changes and fix batch process.

--- titania/includes/manage_tools/update_release_topics.php
+++ titania/includes/manage_tools/update_release_topics.php
@@ -16,12 +16,7 @@ if (!defined('IN_PHPBB'))
 }
 
 class update_release_topics
-{
-	/**
-	* Number of posts to be updated per run
-	*/
-	var $step_size = 100;
-	
+{	
 	/**
 	* Contains the post that is currently updated
 	*/
@@ -42,79 +37,81 @@ class update_release_topics
 	{
 		// Define some vars that we'll need
 		$start	= request_var('start', 0);
+		$total = 0;
+		$limit = 1;
 
 		titania::_include('functions_posting', 'phpbb_posting');
 		titania::add_lang('contributions');
 
+		$sql = 'SELECT COUNT(contrib_id) AS cnt FROM ' . TITANIA_CONTRIBS_TABLE . '
+			WHERE contrib_status = ' . TITANIA_CONTRIB_APPROVED;
+		phpbb::$db->sql_query($sql);
+		$total = phpbb::$db->sql_fetchfield('cnt');
+		phpbb::$db->sql_freeresult();
+				
 		// Greb our batch
 		$sql_ary = array(
 			'SELECT'	=> 'c.contrib_id, c.contrib_user_id, c.contrib_type, c.contrib_name, c.contrib_name_clean, c.contrib_desc, c.contrib_desc_uid, c.contrib_release_topic_id,
-				r.revision_id, r.attachment_id, r.revision_version, p.post_id, a.real_filename, a.filesize',
+				r.revision_id, r.attachment_id, r.revision_version, t.topic_first_post_id, a.real_filename, a.filesize',
 			
 			'FROM'		=> array(
 				TITANIA_CONTRIBS_TABLE	=> 'c',
 				TITANIA_REVISIONS_TABLE => 'r',
-				POSTS_TABLE				=> 'p',
 				TOPICS_TABLE			=> 't',
 				USERS_TABLE				=> 'u',
 				TITANIA_ATTACHMENTS_TABLE => 'a',
 			),
 			
-			'WHERE'		=> "t.topic_id = p.topic_id AND u.user_id = c.contrib_user_id 
+			'WHERE'		=> "u.user_id = c.contrib_user_id 
 				AND c.contrib_release_topic_id = t.topic_id AND c.contrib_id = r.contrib_id
-				AND r.attachment_id = a.attachment_id
-				AND t.topic_first_post_id  = p.post_id",
+				AND r.attachment_id = a.attachment_id",
 		);
 		
 		$sql	= phpbb::$db->sql_build_query('SELECT', $sql_ary);
-		$result	= phpbb::$db->sql_query_limit($sql, $this->step_size, $start);
-		$batch	= phpbb::$db->sql_fetchrowset($result);
-		phpbb::$db->sql_freeresult($result);
+		$result	= phpbb::$db->sql_query_limit($sql, $limit, $start);
 		
-		if (sizeof($batch))
+		while ($row = phpbb::$db->sql_fetchrow($result))
 		{
-			// Walk through the batch
-			foreach ($batch as $this->post)
-			{
-				// Get user data
-				users_overlord::load_users(array($this->post['contrib_user_id']));
-				
-				$contrib = new titania_contribution();
-				$contrib->author = new titania_author();
-				
-				$contrib->__set_array($this->post);
-				$contrib->author->user_id = $contrib->contrib_user_id;
-				
-				$contrib_desc = censor_text($contrib->contrib_desc);
-				titania_decode_message($contrib_desc, $contrib->contrib_desc_uid);
-				
-				$body = sprintf(phpbb::$user->lang[titania_types::$types[$contrib->contrib_type]->create_public],
-					$contrib->contrib_name,
-					$contrib->author->get_url(),
-					users_overlord::get_user($contrib->author->user_id, '_username'),
-					$contrib_desc,
-					$contrib->revision_version,
-					titania_url::build_url('download', array('id' => $this->post['attachment_id'])),
-					$contrib->real_filename,
-					$contrib->filesize,
-					$contrib->get_url(),
-					$contrib->get_url('support')
-				);
-				
-				$options = array(
-					'poster_id'		=> titania_types::$types[$contrib->contrib_type]->forum_robot,
-					'forum_id' 		=> titania_types::$types[$contrib->contrib_type]->forum_database,
-					'topic_id'		=> $contrib->contrib_release_topic_id,
-					'post_id'		=> $contrib->post_id,
-					'topic_title'	=> $contrib->contrib_name,
-					'post_text'		=> $body,
-				);
-				
-				phpbb_posting('edit', $options);
-			}
+			// Get user data
+			users_overlord::load_users(array($row['contrib_user_id']));
+			
+			$contrib = new titania_contribution();
+			$contrib->author = new titania_author();
+			
+			$contrib->__set_array($row);
+			$contrib->author->user_id = $contrib->contrib_user_id;
+			
+			$contrib_desc = $contrib->contrib_desc;
+			titania_decode_message($contrib_desc, $contrib->contrib_desc_uid);
+			
+			$body = sprintf(phpbb::$user->lang[titania_types::$types[$contrib->contrib_type]->create_public],
+				$contrib->contrib_name,
+				$contrib->author->get_url(),
+				users_overlord::get_user($contrib->author->user_id, '_username'),
+				$contrib_desc,
+				$contrib->revision_version,
+				titania_url::build_url('download', array('id' => $row['attachment_id'])),
+				$contrib->real_filename,
+				$contrib->filesize,
+				$contrib->get_url(),
+				$contrib->get_url('support')
+			);
+			
+			$options = array(
+				'poster_id'		=> titania_types::$types[$contrib->contrib_type]->forum_robot,
+				'forum_id' 		=> titania_types::$types[$contrib->contrib_type]->forum_database,
+				'topic_id'		=> $contrib->contrib_release_topic_id,
+				'post_id'		=> $contrib->topic_first_post_id,
+				'topic_title'	=> $contrib->contrib_name,
+				'post_text'		=> $body,
+			);
+			
+			phpbb_posting('edit', $options);
 		}
 		
-		if ($start < $this->step_size)
+		phpbb::$db->sql_freeresult($result);
+		
+		if (($start + $limit) >= $total)
 		{
 			trigger_back('UPDATE_RELEASE_TOPICS_COMPLETE');
 		}
