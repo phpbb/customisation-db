6740f046c71145f6947e4e9044e669dc7b0f0ba6
Nathan Guse <exreaction@phpbb.com>
2010-08-02 00:31:35 +0000

    Adding the bridge type (not tested)

--- language/en/acp/permissions_titania.php
+++ language/en/acp/permissions_titania.php
@@ -128,6 +128,11 @@ $lang = array_merge($lang, array(
 	'acl_u_titania_mod_converter_validate'				=> array('lang' => 'Can validate Converters', 'cat' => 'titania_moderate'),
 	'acl_u_titania_mod_converter_moderate'				=> array('lang' => 'Can moderate Converters', 'cat' => 'titania_moderate'),
 
+	'acl_u_titania_mod_bridge_queue_discussion'			=> array('lang' => 'Can see Bridge Queue Discussion', 'cat' => 'titania_moderate'),
+	'acl_u_titania_mod_bridge_queue'					=> array('lang' => 'Can see Bridge Queue', 'cat' => 'titania_moderate'),
+	'acl_u_titania_mod_bridge_validate'					=> array('lang' => 'Can validate Briges', 'cat' => 'titania_moderate'),
+	'acl_u_titania_mod_bridge_moderate'					=> array('lang' => 'Can moderate Bridges', 'cat' => 'titania_moderate'),
+
 	'acl_u_titania_mod_official_tool_moderate'			=> array('lang' => 'Can submit/moderate Official Tools', 'cat' => 'titania_moderate'),
 
 	'acl_u_titania_admin'			=> array('lang' => 'Can <strong>administrate</strong> Titania', 'cat' => 'titania_moderate'),
