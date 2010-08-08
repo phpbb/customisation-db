08c72926280c5d547ed8b5508ed4d762e96ca4d2
Nathan Guse <exreaction@phpbb.com>
2010-08-08 18:06:24 +0000

    Only show the change owner field on manage contribution if can change owner

--- titania/contributions/manage.php
+++ titania/contributions/manage.php
@@ -343,6 +343,7 @@ phpbb::$template->assign_vars(array(
 	'S_POST_ACTION'				=> titania::$contrib->get_url('manage'),
 	'S_EDIT_SUBJECT'			=> (titania_types::$types[titania::$contrib->contrib_type]->acl_get('moderate')) ? true : false,
 	'S_DELETE_CONTRIBUTION'		=> (phpbb::$auth->acl_get('u_titania_admin')) ? true : false,
+	'S_IS_OWNER'				=> (titania::$contrib->is_author) ? true : false,
 	'S_IS_MODERATOR'			=> (titania_types::$types[titania::$contrib->contrib_type]->acl_get('moderate')) ? true : false,
 	'S_CAN_EDIT_STYLE_DEMO'		=> (titania::$config->can_modify_style_demo_url || titania_types::$types[TITANIA_TYPE_STYLE]->acl_get('moderate') || titania::$contrib->contrib_type != TITANIA_TYPE_STYLE) ? true : false,
 
