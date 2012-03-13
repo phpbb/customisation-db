abd87ded555f21949ac84bb7f2b8437a042979d4
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-03-13 21:40:16 +0000

    Fixing up bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -46,7 +46,8 @@ require_once __DIR__ . '/test_framework/cdb_database_test_case.php';
 require_once __DIR__ . '/test_framework/cdb_database_test_connection_manager.php';
 require_once __DIR__ . '/test_framework/cdb_test_case.php';
 
-require_once PHPBB_FILES . 'includes/class_loader.' . $phpEx;
+require_once '../titania/common.php';
+
 $cdb_class_loader = new phpbb_class_loader('cdb_', CDBPATH);
 $cdb_class_loader->register();
 $phpbb_class_loader = new phpbb_class_loader('phpbb_', PHPBB_FILES . 'includes/');
