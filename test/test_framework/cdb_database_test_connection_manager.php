ae56b9814668288d8378d23fc7dd24761d9f99a8
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-03-13 22:47:28 +0000

    Adding load schema for titania db

--- test/test_framework/cdb_database_test_connection_manager.php
+++ test/test_framework/cdb_database_test_connection_manager.php
@@ -6,4 +6,11 @@ class cdb_database_test_connection_manager extends phpbb_database_test_connectio
 	{
 		parent::__construct($config);
 	}
+
+	public function load_schema()
+	{
+	   parent::load_schema();
+
+	   // We'll add our stuff here later
+	}
 }
