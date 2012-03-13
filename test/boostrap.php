54ea9399cab9716a466246d42830046b3cb960e1
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-03-13 19:50:09 +0000

    [task/testsuite] Add test suite to titania

---
+++ test/boostrap.php
@@ -0,0 +1,53 @@
+<?php
+/**
+*
+* Titania Test Suite
+* 
+* @package testing
+* @copyright (c) 2012 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+// Define some initial constants
+define('CDBPATH', __DIR__ . '/../titania/');
+define('PHPBB_FILES', CDBPATH . 'phpBB/');
+define('IN_TEST', true);
+
+// Some to make phpBB files accessable in the first place
+$phpbb_root_path = CDBPATH . '../';
+$phpEx = 'php';
+define('IN_PHPBB', true);
+
+// Map the db constants to the phpBB vars
+if (!defined('dbms'))
+{
+	define('dbms', 'sqlite');
+	define('dbhost', __DIR__ . '/unit_tests.sqlite2'); // filename
+	define('dbuser', '');
+	define('dbpasswd', '');
+	define('dbname', '');
+	define('dbport', '');
+	define('table_prefix', '');
+}
+$dbms = dbms;
+
+$phpbb_tests_path = CDBPATH . 'vendor/phpBB/tests/';
+$phpEx = 'php';
+
+$table_prefix = (!defined('table_prefix')) ? 'phpbb_' : table_prefix;
+
+require_once $phpbb_tests_path . 'test_framework/phpbb_test_case_helpers.php';
+require_once $phpbb_tests_path . 'test_framework/phpbb_test_case.php';
+require_once $phpbb_tests_path . 'test_framework/phpbb_database_test_case.php';
+require_once $phpbb_tests_path . 'test_framework/phpbb_database_test_connection_manager.php';
+
+require_once __DIR__ . '/test_framework/cdb_database_test_case.php';
+require_once __DIR__ . '/test_framework/cdb_database_test_connection_manager.php';
+require_once __DIR__ . '/test_framework/cdb_test_case.php';
+
+require_once PHPBB_FILES . 'includes/class_loader.' . $phpEx;
+$cdb_class_loader = new phpbb_class_loader('cdb_', CDBPATH);
+$cdb_class_loader->register();
+$phpbb_class_loader = new phpbb_class_loader('phpbb_', PHPBB_FILES . 'includes/');
+$phpbb_class_loader->register();
---
+++ test/test_framework/cdb_database_test_connection_manager.php
@@ -0,0 +1,9 @@
+<?php
+
+class cdb_database_test_connection_manager extends phpbb_database_test_connection_manager
+{
+	public function __construct($config)
+	{
+		parent::__construct($config);
+	}
+}
