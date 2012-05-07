72d70cedaf26ca1a6021aa5aec70aa5ebdca1d48
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-05-07 10:42:10 +0100

    Move stuff to the extension

---
+++ ext/phpBB/titania/language/en/info_mcp_titania.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* @package Titania
+* @copyright (c) 2008 phpBB Customisation Database Team
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+// Create the lang array if it does not already exist
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// Merge the following language entries into the lang array
+$lang = array_merge($lang, array(
+	'MCP_TITANIA'				=> 'Titania',
+	'MCP_TITANIA_ATTENTION'		=> 'Titania Attention',
+));
+
+?>
