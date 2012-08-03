f795913a2ba72eeb5b614db9f47473b4968c51a4
nextgen666 <neodarknsf@gmail.com>
2012-08-03 14:06:25 -0600

    files fixed

--- titania/language/es/types/style.php
+++ titania/language/es/types/style.php
@@ -5,7 +5,8 @@
 
 * @copyright (c) 2008 phpBB Customisation Database Team
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
-*
+* Traducción hecha y revisada por nextgen <http://www.melvingarcia.com>
+* Traductores anteriores angelismo y sof-teo
 */
 
 /**
