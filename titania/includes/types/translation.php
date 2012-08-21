9ecb27bab3855f8dd438a0636d13079675545d24
Cesar G <prototech91@gmail.com>
2012-08-20 19:27:45 -0700

    Translation zip file renamer should use contrib_name_clean as requested by Mael.

--- titania/includes/types/translation.php
+++ titania/includes/types/translation.php
@@ -223,7 +223,7 @@ class titania_type_translation extends titania_type_base
 	*/		
 	public function fix_package_name($contrib, $revision, $revision_attachment)
 	{
-		$new_real_filename = titania_url::url_slug($contrib->contrib_name) . '_' . preg_replace('#[^0-9a-z]#', '_', strtolower($revision->revision_version)) . '.' . $revision_attachment->extension;
+		$new_real_filename = titania_url::url_slug($contrib->contrib_name_clean) . '_' . preg_replace('#[^0-9a-z]#', '_', strtolower($revision->revision_version)) . '.' . $revision_attachment->extension;
 
 		$revision_attachment->change_real_filename($new_real_filename);
 	}
