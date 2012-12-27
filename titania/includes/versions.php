f22d74323dd7f8892c446bf097cdf163a35e6f40
Cesar G <prototech91@gmail.com>
2012-12-27 13:35:52 -0800

    Filesizes and hashes aren't recalculated after cleaning revisions. Bug #63284

--- titania/includes/versions.php
+++ titania/includes/versions.php
@@ -750,5 +750,8 @@ $versions = array(
 			array(TITANIA_ATTENTION_TABLE, 'notify_reporter', array('TINT:1', 0)),
 		),
 	),
+	'0.5.9'	=> array(
+		'custom'	=> 'titania_custom',
+	),
 	// IF YOU ADD A NEW VERSION DO NOT FORGET TO INCREMENT THE VERSION NUMBER IN common.php!
 );
