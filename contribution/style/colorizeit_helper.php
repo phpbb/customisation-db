9947f3f9e97d78d574a2417bec02a5636f0e58f2
Cesar G <prototech91@gmail.com>
2014-10-01 23:48:26 -0700

    Add ColorizeIt controller.

---
+++ contribution/style/colorizeit_helper.php
@@ -0,0 +1,333 @@
+<?php
+/**
+*
+* This file is part of the phpBB Customisation Database package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+namespace phpbb\titania\contribution\style;
+
+class colorizeit_helper
+{
+	/** @var array */
+	protected $components;
+
+	/**
+	* Construct.
+	*/
+	public function __construct()
+	{
+		$this->components = array(
+			'imageset',
+			'template',
+			'theme',
+		);
+	}
+
+	/**
+	* Generate ColorizeIt options for a revision.
+	*
+	* @param string $zip_file	Full path to revision zip file
+	* @throws \Exception		Throws exception if zip file does not exist
+	* @return array
+	*/
+	public function generate_options($zip_file)
+	{
+		if (!@file_exists($zip_file))
+		{
+		    throw new \Exception('ERROR_NO_ATTACHMENT');
+		}
+
+		$new_dir_name = md5(gen_rand_string(64)) . '_' . microtime();
+		$contrib_tools = new \titania_contrib_tools($zip_file, $new_dir_name);
+		$data = $this->get_data($contrib_tools->unzip_dir);
+		$contrib_tools->remove_temp_files();
+
+		return $data;
+	}
+
+	/**
+	* Set ColorizeIt options for a revision.
+	*
+	* @param array $options
+	* @param int $revision_id
+	* @param \phpbb\db\driver\driver_interface $db
+	*
+	* @return null
+	*/
+	public function submit_options($options, $revision_id, $db)
+	{
+		$options = serialize($options);
+		$sql = 'UPDATE ' . TITANIA_REVISIONS_TABLE . '
+		    SET revision_clr_options = "' . $db->sql_escape($options) . '"
+		    WHERE revision_id = ' . (int) $revision_id;
+		$db->sql_query($sql);
+	}
+
+	/**
+	* Get data to be provided to ColorizeIt from style.
+	*
+	* @param string $directory		Full style directory path
+	* @return array
+	*/
+	protected function get_data($directory)
+	{
+		$cfg_files = $this->find_cfg_files($directory);
+
+		$no_result = array(
+			'parser'	=> 'default',
+			'options'	=> false,
+		);
+
+		// No cfg files were found. Not a phpBB style
+		if (empty($cfg_files))
+		{
+			return $no_result;
+		}
+
+		$info = $this->get_style_info($cfg_files);
+		extract($info);
+
+		if (empty($properties))
+		{
+			return $no_result;
+		}
+
+		$has_style = !empty($properties['style']);
+		$lang_imagesets = $this->get_valid_lang_imagesets($lang_imagesets, $imageset);
+
+		if ($has_style)
+		{
+			$properties = $this->remove_clashing_components($properties);
+		}
+
+		$result = $this->get_properties_result($properties, array(
+			'parser'	=> 'phpbb3',
+			'options'	=> true,
+		));
+
+		if (!empty($lang_imagesets))
+		{
+			$result['phpbb3_lang'] = implode(',', $lang_imagesets);
+		}
+
+		if ($has_style && sizeof($properties['style']) == 1)
+		{
+		    $result['style_name'] = end($properties['style']);
+		}
+
+		return $result;
+	}
+
+	/**
+	* Find all .cfg files in a given directory.
+	*
+	* @param string $directory		Full path to style directory
+	* @return array
+	*/
+	protected function find_cfg_files($directory)
+	{
+		$finder = new \phpbb\finder(new \phpbb\filesystem);
+		return $finder
+			->suffix('.cfg')
+			->find_from_paths(array('/' => $directory))
+		;
+	}
+
+	/**
+	* Get style info from .cfg files.
+	*
+	* @param array $cfg_files
+	* @return array Returns array in form of
+	*	array(
+	*		'properties' => array,
+	*		'lang_imagesets' => array,
+	*		'imageset' array|bool,
+	*	)
+	*
+	*/
+	protected function get_style_info($cfg_files)
+	{
+		$properties = array();
+		$expected = array('style.cfg', 'theme.cfg', 'template.cfg', 'imageset.cfg');
+		$imageset = false;
+		$lang_imagesets = array();
+
+		foreach ($cfg_files as $file)
+		{
+			extract($file);
+	  
+			if (!in_array($filename, $expected))
+			{
+				continue;
+			}
+
+			$property = str_replace('.cfg', '', $filename);
+			$property_dir = $this->get_style_dir($path, false);
+			$is_component = in_array($property, $this->components);
+			$style_dir = $this->get_style_dir($path, $is_component);
+
+			if ($is_component && $property_dir != $property)
+			{
+				if ($property == 'imageset')
+				{
+					$lang_imagesets[] = $file;
+				}
+		        continue;
+			}
+			$name = $this->get_name_from_cfg($path . $filename);
+
+			if ($name !== false)
+			{
+				$properties['dir'][$style_dir] = $style_dir;
+				$properties[$property][$name] = $name;
+
+				if ($property == 'imageset')
+				{
+					$imageset = $file;
+				}
+			}
+		}
+
+		return array(
+			'properties'		=> $properties,
+			'imageset'			=> $imageset,
+			'lang_imagesets'	=> $lang_imagesets,
+		);
+	}
+
+	/**
+	* Get style|component name from configuration file.
+	*
+	* @param string $file		Full path to .cfg file
+	* @return bool|string Returns style|component name, or false
+	*	on failure
+	*/
+	protected function get_name_from_cfg($file)
+	{
+		$result = parse_cfg_file($file);
+		return (empty($result['name'])) ? false : $result['name'];
+	}
+
+	/**
+	* Get style directory name from path.
+	*
+	* @param string $directory	Full style directory path.
+	* @param bool $is_component	Whether the full path leads to a component.
+	*
+	* @param string
+	*/
+	protected function get_style_dir($directory, $is_component = false)
+	{
+		$dirs = explode('/', rtrim($directory, '/'));
+		$count = sizeof($dirs);
+
+		if (!$count)
+		{
+			return '';
+		}
+		$dir_num = $count - (($is_component) ? 2 : 1);
+
+		return $dirs[$dir_num];
+	}
+
+	/**
+	* Define result for given style properties.
+	*
+	* @param array $properties
+	* @param array $result
+	* @return array
+	*/
+	protected function get_properties_result($properties, $result)
+	{
+		foreach ($properties as $property => $instances)
+		{
+			$instances = array_values($instances);
+
+			foreach ($instances as $index => $name)
+			{
+				$id = ($index > 0) ? $index : '';
+				$result["phpbb3_{$property}{$id}"] = $name;
+			}
+		}
+		return $result;
+	}
+
+	/**
+	* Validate language imagesets to ensure that they're under the main imageset directory.
+	*
+	* @param array $lang_imagesets
+	* @param array $imageset
+	*
+	* @return array Returns valid language directories - for example en.
+	*/
+	protected function get_valid_lang_imagesets($lang_imagesets, $imageset)
+	{
+		if (empty($lang_imagesets) || empty($imageset))
+		{
+			return array();
+		}
+
+		$valid = array();
+		$imageset_path = $this->get_imageset_path($imageset['named_path']);
+
+		foreach ($lang_imagesets as $index => $lang)
+		{
+			$lang_path = $this->get_imageset_path($lang['named_path']);
+			$subpath = substr($lang_path, strlen($imageset_path . '/'));
+
+			if (strpos($lang_path, $imageset_path) === 0 && preg_match('#^[a-zA-Z0-9\-_]+$#', $subpath))
+			{
+				$valid[] = $subpath;
+			}
+		}
+
+		return $valid;
+	}
+
+	/**
+	* Get imageset from named imageset.cfg path.
+	*
+	* @param string $named_path
+	* @return string
+	*/
+	protected function get_imageset_path($named_path)
+	{
+		return substr($named_path, 0, - strlen('/imageset.cfg'));
+	}
+
+	/**
+	* Remove component instances whose name matches the style name.
+	*
+	* @param array $properties		Style properties.
+	*
+	* @return array Returns $properties with the clashing component instances removed.
+	*/
+	protected function remove_clashing_components($properties)
+	{
+		$components = array_intersect_key($properties, array_flip($this->components));
+
+		foreach ($components as $component => $instances)
+		{
+			foreach ($instances as $name)
+			{
+				if (isset($properties['style'][$name]))
+				{
+					unset($properties[$component][$name]);
+				}
+			}
+
+			if (empty($properties[$component]))
+			{
+				unset($properties[$component]);
+			}
+		}
+		return $properties;
+	}
+}
---
+++ controller/colorizeit.php
@@ -0,0 +1,309 @@
+<?php
+/**
+*
+* This file is part of the phpBB Customisation Database package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+namespace phpbb\titania\controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use phpbb\request\request_interface;
+use phpbb\titania\contribution\style\colorizeit_helper;
+
+class colorizeit
+{
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/** @var \phpbb\request\request_inteface */
+	protected $request;
+
+	/** @var \phpbb\titania\controller\helper */
+	protected $helper;
+
+	/** @var \titania_config */
+	protected $ext_config;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\db\driver\driver_interface $db
+	* @param \phpbb\user $user
+	* @param \phpbb\request\request_inteface $request
+	* @param \phpbb\titania\controller\helper $helper
+	* @param \titania_config $ext_config
+	*/
+	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\user $user, \phpbb\request\request_interface $request, \phpbb\titania\controller\helper $helper, \titania_config $ext_config)
+	{
+		$this->db = $db;
+		$this->user = $user;
+		$this->request = $request;
+		$this->helper = $helper;
+		$this->ext_config = $ext_config;
+
+		$this->user->add_lang('viewtopic');
+	}
+
+	/**
+	* Output ColorizeIt data for a given revision in XML format.
+	*
+	* @param int $id		Attachment id.
+	* @return Symfony\Component\HttpFoundation\Response
+	*/
+	public function colorizeit_data($id)
+	{
+		if (!strlen($this->ext_config->colorizeit) || !$this->check_auth())
+		{
+			return $this->error('ERROR_NO_ATTACHMENT');
+		}
+
+		try
+		{
+			$this->load($id);
+		}
+		catch (\Exception $e)
+		{
+			return $this->error($e->getMessage());
+		}
+
+		if (!$this->contrib->has_colorizeit())
+		{
+			return $this->error('CLR_ERROR_NOSAMPLE');
+		}
+
+		if (!empty($this->revision->revision_clr_options))
+		{
+			$options = unserialize($this->revision->revision_clr_options);
+		}
+		else
+		{
+			$colorizeit_helper = new colorizeit_helper;
+
+			try
+			{
+				$options = $colorizeit_helper->generate_options($this->attachment->get_filepath());
+			}
+			catch (\Exception $e)
+			{
+				return $this->error($e->getMessage());
+			}
+			$colorizeit_helper->submit_options($options, $this->revision->revision_id, $this->db);
+		}
+
+		return new Response($this->get_xml_output($options), 200);
+	}
+
+	/**
+	* Check authorization.
+	*
+	* @return bool Returns true if user is authorized to access page.
+	*/
+	protected function check_auth()
+	{
+		$var_name = $this->ext_config->colorizeit_var;
+
+		if ($this->ext_config->colorizeit_auth == 'HEADER')
+		{
+			$var_name = 'HTTP_' . strtoupper(str_replace('-', '_', $var_name));
+			$var_value = $this->request->server($var_name);
+		}
+		else
+		{
+			$var_value = $this->request->variable($var_name, '', false, request_interface::POST);
+		}
+		return $var_value === $this->ext_config->colorizeit_value;
+	}
+
+	/**
+	* Get error response.
+	*
+	* @param string $error		Error to output
+	* @return Symfony\Component\HttpFoundation\Response
+	*/
+	protected function error($error)
+	{
+		$error = $this->user->lang($error);
+		$response = '<?xml version="1.0" encoding="UTF-8"?>' . "\n<error>$error</error>";
+
+		return new Response($response);
+	}
+
+	/**
+	* Load attachment, revision, and contribution needed to output
+	* ColorizeIt info.
+	*
+	* @param int $attachment_id		Attachment id.
+	* @return null
+	*/
+	protected function load($attachment_id)
+	{
+		$this->load_attachment($attachment_id);
+		$this->load_revision();
+		$this->load_contribution();
+	}
+
+	/**
+	* Load attachment.
+	*
+	* @param int $id		Attachment id.
+	* @throws \Exception	Throws exception if attachment does not exist
+	*	or cannot be accessed.
+	* @return null
+	*/
+	protected function load_attachment($id)
+	{
+		$id = (int) $id;
+		$this->attachment = new \titania_attachment(TITANIA_CONTRIB);
+
+		if (!$id || !$this->attachment->load($id) || !$this->check_attachment_auth())
+		{
+			throw new \Exception('ERROR_NO_ATTACHMENT');
+		}
+	}
+
+	/**
+	* Check whether user can access attachment.
+	*
+	* @return bool
+	*/
+	protected function check_attachment_auth()
+	{
+		return $this->attachment->object_type == TITANIA_CONTRIB &&
+			!$this->attachment->is_orphan &&
+			$this->attachment->attachment_access == TITANIA_ACCESS_PUBLIC;
+	}
+
+	/**
+	* Load revision.
+	*
+	* @throws \Exception	Throws \Exception if revision cannot be loaded.
+	* @return null
+	*/
+	protected function load_revision()
+	{
+		$sql = 'SELECT revision_id, contrib_id, revision_status, revision_clr_options
+			FROM ' . TITANIA_REVISIONS_TABLE . '
+    		WHERE  attachment_id = ' . (int) $this->attachment->attachment_id;
+    	$result = $this->db->sql_query($sql);
+    	$data = $this->db->sql_fetchrow($result);
+    	$this->db->sql_freeresult();
+
+		if (!$data || $data['revision_status'] != TITANIA_REVISION_APPROVED)
+		{
+			throw new \Exception('NO_REVISION');
+		}
+		$this->revision = new \titania_revision(false);
+		$this->revision->__set_array($data);
+	}
+
+	/**
+	* Load contribution.
+	*
+	* @throws \Exception	Throws \Exception if contribution cannot be loaded.
+	* @return null
+	*/
+	protected function load_contribution()
+	{
+		$this->contrib = new \titania_contribution;
+
+		if (!$this->contrib->load($this->revision->contrib_id) || !$this->contrib->is_visible())
+		{
+			throw new \Exception('NO_CONTRIB');
+		}
+	}
+
+	/**
+	* Generate XML data ouput.
+	*
+	* @param array $data
+	* @return string
+	*/
+	protected function get_xml_output($data)
+	{
+		$tab = "\t";
+		$newline = "\n";
+		$properties = array(
+			'name'				=> $this->contrib->contrib_name,
+			'file_src'			=> $this->get_file_source($this->attachment->attachment_id),
+			'file_created'		=> $this->attachment->filetime,
+			'sample_src'		=> $this->get_file_source($this->contrib->clr_sample['attachment_id']),
+			'sample_created' 	=> $this->contrib->clr_sample['filetime'],
+			'colors'			=> $this->contrib->contrib_clr_colors,
+			'parser'			=> $data['parser'],
+		);
+
+		$properties = array_map('htmlspecialchars', $properties);
+
+		$xml =
+			'<?xml version="1.0" encoding="UTF-8"?>'		. $newline .
+			'<colorizeit>'									. $newline .
+			$tab . '<name>%1$s</name>'						. $newline .
+			$tab . '<file src="%2$s" created="%3$s" />'		. $newline .
+			$tab . '<sample src="%4$s" created="%5$s" />'	. $newline .
+			$tab . '<colors>%6$s</colors>'					. $newline .
+			$tab . '<parser>%7$s</parser>'					. $newline
+		;
+		$xml = vsprintf($xml, $properties);
+
+		if ($data['options'])
+		{
+			foreach($data as $key => $value)
+			{
+				if ($this->is_valid_option($data['parser'], $key))
+				{
+					$xml .= $tab . '<option name="' . htmlspecialchars($key) . '">' . htmlspecialchars($value) . '</option>' . $newline;
+				}
+			}
+
+			if (isset($data['phpbb3_dir']))
+			{
+				$xml .= $tab . '<filename>' . htmlspecialchars($data['phpbb3_dir']) . '.zip</filename>' . $newline;
+			}
+		}
+
+		$xml .= '</colorizeit>';
+
+		return $xml;
+	}
+
+	/**
+	* Get absolute path to an attachment.
+	*
+	* @param int $id		Attachment id.
+	* @return string
+	*/
+	protected function get_file_source($id)
+	{
+		return $this->helper->route(
+			'phpbb.titania.download',
+			array('id' => $id),
+			true,
+			false,
+			UrlGeneratorInterface::ABSOLUTE_PATH
+		);
+	}
+
+	/**
+	* Check whether the given option is valid for the parser.
+	*
+	* @param string $parser
+	* @param string $option
+	*
+	* @return bool
+	*/
+	protected function is_valid_option($parser, $option)
+	{
+		return strpos($option, $parser) === 0;
+	}
+}
