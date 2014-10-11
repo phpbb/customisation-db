fa8141f35e70157e7c92814c792cf0058bf91222
Cesar G <prototech91@gmail.com>
2014-10-11 07:30:07 -0700

    Update translation update steps to new format.

--- language/en/contributions.php
+++ language/en/contributions.php
@@ -161,6 +161,7 @@ $lang = array_merge($lang, array(
 	'PHPBB_BRANCH'							=> 'phpBB Branch',
 	'PHPBB_BRANCH_EXPLAIN'					=> 'Select the phpBB branch that this revision supports.',
 	'PHPBB_VERSION'							=> 'phpBB Version(s)',
+	'PV_TEST'								=> 'Prevalidator test',
 	'PV_RESULTS'							=> '<strong>Please check over the prevalidator results and make sure that nothing needs to be fixed.<br /><br />If you do not think anything requires fixing or you are not sure, just hit continue below.</strong>',
 
 	'QUEUE_ALLOW_REPACK'					=> 'Allow Repacking',
