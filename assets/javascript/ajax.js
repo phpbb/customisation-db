a35672bdad939b0d1b088178f700f191a730ca99
Cesar G <prototech91@gmail.com>
2015-07-01 08:56:20 -0700

    Refactor quick edit to use phpBB's AJAX implementation.

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -2,10 +2,59 @@
 
 	'use strict';
 
+	/**
+	 * Filter quick edit AJAX requests.
+	 * Prevents requests when form is already open.
+	 *
+	 * @param {object} data
+	 * @param {object} event
+	 * @returns {boolean}
+	 */
+	titania.quickEditFilter = function(data, event) {
+		var $postbody = $(this).parents('.postbody');
+
+		if ($('form', $postbody).length) {
+			event.preventDefault();
+			return false;
+		}
+		return true;
+	};
+
+	/**
+	 * Filter quick edit form AJAX submissions.
+	 * Ensures that the form is submitted through AJAX only when the
+	 * Submit button is clicked.
+	 *
+	 * @param {object} data
+	 * @returns {boolean}
+	 */
+	titania.quickEditSubmitFilter = function(data) {
+		return $(this).find('input:submit[data-clicked]').attr('name') === 'submit';
+	};
+
 	phpbb.addAjaxCallback('titania.demo.install', function(res) {
 		if (typeof res.url !== 'undefined' && res.url !== '') {
 			$('#demo_url_' + $(this).data('branch')).val(res.url);
 		}
 	});
 
+	phpbb.addAjaxCallback('titania.quick_edit', function(response) {
+		var $postbody = $(this).parents('.postbody'),
+			$post = $postbody.find('.content');
+
+		// Store the original post in case the user cancels the edit
+		$post.after($post.clone());
+		$post.next().addClass('hidden original_post');
+		$post.replaceWith(response.form);
+		titania.ajaxify($postbody.find('form'));
+	});
+
+	phpbb.addAjaxCallback('titania.quick_edit.submit', function(response) {
+		var $form = $(this),
+			$postbody = $form.parents('.postbody');
+
+		$form.replaceWith('<div class="content text-content">' + response.message + '</div>');
+		$('h3 a', $postbody).html(response.subject);
+		$('.original_post', $postbody).remove();
+	});
 })(jQuery); // Avoid conflicts with other libraries
--- assets/javascript/common.js
+++ assets/javascript/common.js
@@ -151,6 +151,26 @@ titania.updateRating = function($stars, rating, rated) {
 	});
 };
 
+titania.ajaxify = function($el) {
+	$el.find('[data-ajax]').addBack('[data-ajax]').each(function() {
+		var $this = $(this);
+		var ajax = $this.attr('data-ajax');
+		var filter = $this.attr('data-filter');
+
+		if (ajax !== 'false') {
+			var fn = (ajax !== 'true') ? ajax : null;
+			filter = (filter !== undefined) ? phpbb.getFunctionByName(filter) : null;
+
+			phpbb.ajaxify({
+				selector: this,
+				refresh: $this.attr('data-refresh') !== undefined,
+				filter: filter,
+				callback: fn
+			});
+		}
+	});
+};
+
 /**
 * Highlight rating stars when hovering over them
 */
@@ -195,115 +215,15 @@ $('.contrib-download').hover(function() {
 	$('.download-info', this).fadeOut('slow');
 });
 
-	// Ajax Quick Edit
-	$('.postbody > .post-buttons .edit-icon').click(function(e) {
-		var postbody = $(this).parents('.postbody');
-		var full_edit = $(this).attr('href');
-
-		// Return false if the form is already open
-		if ($('form', postbody).length || $('.qe-error', postbody).length)
-		{
-			e.preventDefault();
-			return;
-		}
-
-		var post = $(postbody).children('.content');
-
-		// Store the original post in case the user cancels the edit
-		$(post).after($(post).clone());
-		$(post).next().addClass('hidden original_post');
-
-		// Ajax time
-		$.ajax({
-			type: "POST",
-			url: $(post).parent().children('.quick_edit').val(),
-			success: function(response){
-
-				// If a full page was served, then an error occurred. Redirect to full edit.
-				if (response.indexOf('{') !== 0)
-				{
-					window.location.href = full_edit;
-				}
-
-				var response = eval('(' + response + ')');
-
-				if (response.result == 'error')
-				{
-					$(post).before('<div class="error qe-error">' + response.content + '</div>');
-					return;
-				}
-
-				$(post).replaceWith(response.content);
-
-				var quickeditor = $(postbody).children('form').children('textarea');
-
-				$(quickeditor).parent().children('.submit-buttons').children('[name=submit]').click(function(e) {
-
-					// Ajax time
-					$.ajax({
-						type: "POST",
-						url: $(quickeditor).parent().attr('action'),
-						data: $(quickeditor).parent().serialize() + '&submit=1',
-						success: function(response){
-							var qe_form = $(quickeditor).parent();
-	
-							// If a full page was served, then an error occurred.
-							if (response.indexOf('{') !== 0)
-							{
-								$(qe_form).before('<div class="error qe-error">' + form_error + '</div>');
-							}
-							else
-							{
-								var response = eval('(' + response + ')');
-
-								// If the form token is invalid, redirect to full edit.
-								if (response.result == 'invalid_token')
-								{
-									$('[name=full_editor]', qe_form).trigger('click');
-									
-								}
-								else if (response.result == 'error')
-								{
-									$(qe_form).before('<div class="error qe-error">' + response.content + '</div>');
-								}
-								else
-								{
-									// Hide the form only if the AJAX call succeeded
-									$(qe_form).hide();
-
-									$(qe_form).replaceWith('<div class="content text-content">' + response.content + '</div>');
-									var subject = $('.content:not(.original_post)', postbody).children('span:first-child');
-									$('h3 a', postbody).html($(subject).html());
-									$(subject).remove();
-									$('.original_post', postbody).remove();
-								}
-							}
-						}
-					});
-
-					// Do not redirect
-					e.preventDefault();
-				});
-			},
-			error: function() {
-				window.location.href = full_edit;
-			}
-		});
-
-		// Do not follow the link
-		e.preventDefault();
-	});
-
 	// Canceled quick edit, so display original post again
-	$(document).on('click', '.postbody #cancel', function(event) {
+	$('.postbody').on('click', '#cancel', function(event) {
 		event.preventDefault();
 
-		var postbody = $(this).parents('.postbody');
+		var $postbody = $(this).parents('.postbody');
 
-		$('form', postbody).remove();
+		$('form', $postbody).remove();
 		// Remove warnings
-		$('.qe-error', postbody).remove();
-		$('.original_post', postbody).removeClass('hidden');
+		$('.original_post', $postbody).removeClass('hidden');
 	});
 
 	$('.download-main').click(function() {
