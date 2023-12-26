6302ea841cb418aae4e8ce8c61c46ef1149eba7c
battye <administration@online-scrabble.com>
2023-12-26 15:30:26 +0800

    First attempt at Codespaces

---
+++ .devcontainer/resources/setup.sh
@@ -0,0 +1,44 @@
+# Copy Apache configuration
+# sudo rm /etc/apache2/sites-enabled/000-default.conf
+# sudo cp .devcontainer/resources/phpbb-apache.conf /etc/apache2/sites-enabled/000-default.conf
+
+# Start MySQL
+sudo service mysql start
+
+# Start Apache
+sudo service apache2 start
+
+# Add SSH key
+echo "$SSH_KEY" > /home/vscode/.ssh/id_rsa && chmod 600 /home/vscode/.ssh/id_rsa
+
+# Create a MySQL user to use
+sudo mysql -u root<<EOFMYSQL
+    CREATE USER 'phpbb'@'localhost' IDENTIFIED BY 'phpbb'; 
+    GRANT ALL PRIVILEGES ON *.* TO 'phpbb'@'localhost' WITH GRANT OPTION;
+    CREATE DATABASE IF NOT EXISTS phpbb;
+EOFMYSQL
+
+# Download dependencies
+echo "Dependencies"
+composer install --no-interaction
+
+# Install phpBB
+echo "phpBB project"
+composer create-project --no-interaction phpbb/phpbb /workspaces/phpbb
+
+# Copy phpBB config
+echo "Copy phpBB config"
+cp /workspaces/customisation-db/.devcontainer/resources/phpbb-config.yml /workspaces/phpbb/install/install-config.yml
+
+echo "Symlink extension"
+sudo rm -rf /var/www/html
+sudo ln -s /workspaces/phpbb /var/www/html
+mkdir /workspaces/phpbb/ext/phpbb
+sudo ln -s /workspaces/customisation-db /workspaces/phpbb/ext/phpbb/titania
+
+echo "phpBB CLI install"
+cd /workspaces/phpbb && composer install --no-interaction
+sudo php /workspaces/phpbb/install/phpbbcli.php install /workspaces/phpbb/install/install-config.yml
+rm -rf /workspaces/phpbb/install 
+
+echo "Completed"
