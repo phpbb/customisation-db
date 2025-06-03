14916ab451cd162c241b7f2655ea3e6c7d7e7a4b
Daniel James <danielt70@icloud.com>
2025-06-03 20:07:19 +0100

    Switch to curly apostrophe in lang string

--- language/en/memberlist.php
+++ language/en/memberlist.php
@@ -35,8 +35,12 @@ if (empty($lang) || !is_array($lang))
 // You do not need this where single placeholders are used, e.g. 'Message %d' is fine
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
 
 $lang = array_merge($lang, array(
 	'USER_TOTAL_CONTRIBUTIONS'			=> 'Total contributions',
-	'SEARCH_USER_CONTRIBUTIONS'			=> 'Search user\'s contributions',
+	'SEARCH_USER_CONTRIBUTIONS'			=> 'Search user’s contributions',
 ));
