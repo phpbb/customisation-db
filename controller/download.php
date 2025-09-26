421e1c2431105c9427ae1d6c6e939ad10b709cea
paulsohier <paul.sohier@qbuzz.nl>
2025-09-26 18:28:08 +0200

    Fixes for phpBB4.

--- controller/download.php
+++ controller/download.php
@@ -87,7 +87,6 @@ class download
         $this->attachements_table = $attachements_table;
 
 		$this->user->add_lang('viewtopic');
-		require($this->phpbb_root_path . 'includes/functions_download.' . $this->php_ext);
 	}
 
 	/**
@@ -95,7 +94,7 @@ class download
 	*
 	* @param int	$id		Attachment id.
 	* @param string	$type	Type of download (manual or composer)
-	* @return \Symfony\Component\HttpFoundation\Response if error found. Otherwise method exits.
+	* @return Symfony\Component\HttpFoundation\BinaryFileResponse\BinaryFileResponse if error found. Otherwise method exits.
 	*/
 	public function file($id , $type)
 	{
@@ -109,7 +108,6 @@ class download
 			$this->id = (int) $this->get_legacy_download_id();
 		}
 
-		$mode = $this->request->variable('mode', '');
 		$thumbnail = $this->request->variable('thumb', false);
 		$status = $this->load_file_data();
 
@@ -126,14 +124,11 @@ class download
 		$directory = utf8_basename($this->file['attachment_directory']) . '/';
 		$base_filename = utf8_basename($this->file['physical_filename']);
 		$is_image = strpos($this->file['mimetype'], 'image') === 0;
-		$is_ie = strpos(strtolower($this->user->browser), 'msie') !== false;
-		$display_cat = ($is_image) ? ATTACHMENT_CATEGORY_IMAGE : ATTACHMENT_CATEGORY_NONE;
 
-		if ($thumbnail && $is_image)
+        if ($thumbnail && $is_image)
 		{
 			$this->file['physical_filename'] = $directory . 'thumb_' . $base_filename;
-			$display_cat = ATTACHMENT_CATEGORY_THUMB;
-		}
+        }
 		else
 		{
 			$this->file['physical_filename'] = $directory . $base_filename;
@@ -152,19 +147,19 @@ class download
 			$this->file['physical_filename'] = $composer_package;
 		}
 
-		if (!$thumbnail && $mode === 'view' && $is_image && $is_ie && !phpbb_is_greater_ie_version($this->user->browser, 7))
-		{
-			$file_url = $this->helper->route('phpbb.titania.download', array('id' => $this->id));
+        $filename = $this->ext_config->upload_path . $this->file['physical_filename'];
 
-			wrap_img_in_html($file_url, $this->file['real_filename']);
-			file_gc();
-		}
-		else
-		{
-			$filename = $this->ext_config->upload_path . $this->file['physical_filename'];
+        if (!@file_exists($filename))
+        {
+            return $this->helper->error('ERROR_NO_ATTACHMENT', 404);
+        }
 
-			return $this->send_file_to_browser($this->file, $filename, $display_cat);
-		}
+        $headers = [
+            'Content-Type' => 'application/octet-stream',
+        ];
+
+        return new \Symfony\Component\HttpFoundation\BinaryFileResponse\BinaryFileResponse($filename, 200, $headers);
+
 	}
 
 	/**
@@ -301,10 +296,10 @@ class download
 			{
 				$status = self::NOT_FOUND;
 			}
-			else if (!download_allowed())
+			/*else if (!download_allowed())
 			{
 				$status = self::FORBIDDEN;
-			}
+			}*/
 			else if ($this->file['attachment_access'] < $this->access->get_level() && $this->access->is_team($this->file['attachment_access']))
 			{
 				$status = self::FORBIDDEN;
@@ -501,145 +496,4 @@ class download
 		$this->db->sql_query($sql);
 	}
 
-	/**
-	* Send file to browser
-	*
-	* Copy of send_file_to_browser() from functions_download.php
-	* with some minor modifications to work correctly in Titania.
-	*
-	* @param array $attachment	Attachment data.
-	* @param string $filename	Full path to the attachment file.
-	* @param int $category		Attachment category.
-	*
-	* @return \Symfony\Component\HttpFoundation\Response if error found. Otherwise method exits.
-	*/
-	protected function send_file_to_browser($attachment, $filename, $category)
-	{
-		if (!@file_exists($filename))
-		{
-			return $this->helper->error('ERROR_NO_ATTACHMENT', 404);
-		}
-
-		// Correct the mime type - we force application/octetstream for all files, except images
-		// Please do not change this, it is a security precaution
-		if ($category != ATTACHMENT_CATEGORY_IMAGE || strpos($attachment['mimetype'], 'image') !== 0)
-		{
-			$attachment['mimetype'] = (strpos(strtolower($this->user->browser), 'msie') !== false || strpos(strtolower($this->user->browser), 'opera') !== false) ? 'application/octetstream' : 'application/octet-stream';
-		}
-
-		if (@ob_get_length())
-		{
-			@ob_end_clean();
-		}
-
-		// Now send the File Contents to the Browser
-		$size = @filesize($filename);
-
-		// To correctly display further errors we need to make sure we are using the correct headers for both (unsetting content-length may not work)
-
-		// Check if headers already sent or not able to get the file contents.
-		if (headers_sent() || !@file_exists($filename) || !@is_readable($filename))
-		{
-			// PHP track_errors setting On?
-			if (!empty($php_errormsg))
-			{
-				return $this->helper->error(
-					$this->user->lang['UNABLE_TO_DELIVER_FILE'] . '<br />' . $this->user->lang('TRACKED_PHP_ERROR', $php_errormsg),
-					self::INTERNAL_SERVER_ERROR
-				);
-			}
-
-			return $this->helper->error('UNABLE_TO_DELIVER_FILE', self::INTERNAL_SERVER_ERROR);
-		}
-
-		// Make sure the database record for the filesize is correct
-		if ($size > 0 && $size != $attachment['filesize'])
-		{
-			// Update database record
-			$sql = 'UPDATE ' . $this->attachements_table . '
-				SET filesize = ' . (int) $size . '
-				WHERE attachment_id = ' . (int) $attachment['attachment_id'];
-			$this->db->sql_query($sql);
-		}
-
-		// Now the tricky part... let's dance
-		header('Pragma: public');
-
-		// Send out the Headers. Do not set Content-Disposition to inline please, it is a security measure for users using the Internet Explorer.
-		header('Content-Type: ' . $attachment['mimetype']);
-
-		if (phpbb_is_greater_ie_version($this->user->browser, 7))
-		{
-			header('X-Content-Type-Options: nosniff');
-		}
-
-		if ($category == ATTACHMENT_CATEGORY_FLASH && request_var('view', 0) === 1)
-		{
-			// We use content-disposition: inline for flash files and view=1 to let it correctly play with flash player 10 - any other disposition will fail to play inline
-			header('Content-Disposition: inline');
-		}
-		else
-		{
-			if (empty($this->user->browser) || ((strpos(strtolower($this->user->browser), 'msie') !== false) && !phpbb_is_greater_ie_version($this->user->browser, 7)))
-			{
-				header('Content-Disposition: attachment; ' . header_filename(htmlspecialchars_decode($attachment['real_filename'])));
-				if (empty($this->user->browser) || (strpos(strtolower($this->user->browser), 'msie 6.0') !== false))
-				{
-					header('expires: -1');
-				}
-			}
-			else
-			{
-				header('Content-Disposition: ' . ((strpos($attachment['mimetype'], 'image') === 0) ? 'inline' : 'attachment') . '; ' . header_filename(htmlspecialchars_decode($attachment['real_filename'])));
-				if (phpbb_is_greater_ie_version($this->user->browser, 7) && (strpos($attachment['mimetype'], 'image') !== 0))
-				{
-					header('X-Download-Options: noopen');
-				}
-			}
-		}
-
-		if ($size)
-		{
-			header("Content-Length: $size");
-		}
-
-		// Close the db connection before sending the file etc.
-		file_gc(false);
-
-		if (!set_modified_headers($attachment['filetime'], $this->user->browser))
-		{
-			// Try to deliver in chunks
-			@set_time_limit(0);
-
-			$fp = @fopen($filename, 'rb');
-
-			if ($fp !== false)
-			{
-				// Deliver file partially if requested
-				if ($range = phpbb_http_byte_range($size))
-				{
-					fseek($fp, $range['byte_pos_start']);
-
-					send_status_line(206, 'Partial Content');
-					header('Content-Range: bytes ' . $range['byte_pos_start'] . '-' . $range['byte_pos_end'] . '/' . $range['bytes_total']);
-					header('Content-Length: ' . $range['bytes_requested']);
-				}
-
-				while (!feof($fp))
-				{
-					echo fread($fp, 8192);
-				}
-				fclose($fp);
-			}
-			else
-			{
-				@readfile($filename);
-			}
-
-			flush();
-		}
-
-		exit;
-	}
 }
-
