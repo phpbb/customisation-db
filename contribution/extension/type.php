159621412d2206a1cf1b193e90504d972d7397f9
Matt Friedman <maf675@gmail.com>
2025-10-03 11:16:41 -0700

    Chech submission branch too if writing comp-installers

--- contribution/extension/type.php
+++ contribution/extension/type.php
@@ -280,7 +280,7 @@ class type extends base
 
 		$ext_name = $data['name'];
 		$data['type'] = 'phpbb-extension';
-		$data = $this->update_phpbb_requirement($data);
+		$data = $this->update_phpbb_requirement($data, $revision);
 		$data = $this->set_version_check($data, $contrib);
 
 		$data = json_encode(
@@ -352,9 +352,10 @@ class type extends base
 	 * Updates phpBB requirements in composer.json
 	 *
 	 * @param array $data composer.json data
+	 * @param \titania_revision $revision
 	 * @return array Returns $data array with phpBB requirement updated
 	 */
-	protected function update_phpbb_requirement(array $data)
+	protected function update_phpbb_requirement(array $data, \titania_revision $revision)
 	{
 		if (!isset($data['require']['phpbb/phpbb']))
 		{
@@ -374,7 +375,7 @@ class type extends base
 		if (!isset($data['require']['composer/installers']))
 		{
 			$installers_version = '~1.0';
-			if (isset($data['require']['phpbb/phpbb']) && $this->requires_phpbb_4_or_higher($data['require']['phpbb/phpbb']))
+			if ($this->is_phpbb_4_branch($revision) || (isset($data['require']['phpbb/phpbb']) && $this->requires_phpbb_4($data['require']['phpbb/phpbb'])))
 			{
 				$installers_version = '^1.0 || ^2.0';
 			}
@@ -401,7 +402,7 @@ class type extends base
 	 * @param string $constraint Version constraint
 	 * @return bool True if constraint requires phpBB 4.0.0+
 	 */
-	protected function requires_phpbb_4_or_higher($constraint)
+	protected function requires_phpbb_4($constraint)
 	{
 		try
 		{
@@ -416,6 +417,17 @@ class type extends base
 		{
 			return false;
 		}
+	}
+
+	/**
+	 * Check if revision is submitted to phpBB 4.0+ branch
+	 *
+	 * @param \titania_revision $revision
+	 * @return bool True if submitted to phpBB 4.0+ branch
+	 */
+	protected function is_phpbb_4_branch(\titania_revision $revision)
+	{
+		return max(array_column($revision->phpbb_versions, 'phpbb_version_branch')) >= 40;
 	}
 
 	/**
